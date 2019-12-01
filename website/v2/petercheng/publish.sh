51602d44734001291a0933db33c0d4aaa53b24b4
Peter Cheng <petercheng00@gmail.com>
2019-12-01 15:45:07 -0800

    updates

--- website/v2/petercheng/publish.sh
+++ website/v2/petercheng/publish.sh
@@ -5,7 +5,7 @@ set -eux
 username=$1
 server=ssh.phx.nearlyfreespeech.net
 
-rm -r public
+rm -rf public
 hugo
 # rsync is problematic on WSL (https://github.com/microsoft/WSL/issues/2138)
 # --whole-file is one workaround
