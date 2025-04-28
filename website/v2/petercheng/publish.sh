eb510697c867317bee9443d9f03802ac1d28a739
Peter Cheng <petercheng00@gmail.com>
2025-04-27 22:54:49 -0700

    maybe rsync works now

--- website/v2/petercheng/publish.sh
+++ website/v2/petercheng/publish.sh
@@ -7,7 +7,4 @@ server=ssh.nyc1.nearlyfreespeech.net
 
 rm -rf public resources
 hugo
-# rsync is problematic on WSL (https://github.com/microsoft/WSL/issues/2138)
-# --whole-file is one workaround
-# Not being able to use -z is another limitation
-rsync -avh --whole-file --progress --delete public/ "$username@$server:"
+rsync -avzch --progress --delete public/ "$username@$server:"
